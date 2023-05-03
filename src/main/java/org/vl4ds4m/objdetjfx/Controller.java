b2f0a63d5ac07a8a3668429751d0ef6f3c2b10fa
Vladislav Samsonov <vl4d.s4m@gmail.com>
2023-05-03 18:00:09 +0300

    Improve user interface

--- src/main/java/org/vl4ds4m/objdetjfx/Controller.java
+++ src/main/java/org/vl4ds4m/objdetjfx/Controller.java
@@ -1,8 +1,7 @@
 package org.vl4ds4m.objdetjfx;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
 
 /**
  * This class manage the application.
@@ -11,18 +10,4 @@ import javafx.scene.control.Button;
  * the properties of class fields and the ways to call class methods.
  */
 public class Controller {
-    @FXML
-    private Button imageAdditionButton;
-
-    @FXML
-    private void initialize() {
-    }
-
-    @FXML
-    private void clickOnImageAdditionButton() {
-        Alert alert = new Alert(Alert.AlertType.WARNING);
-        alert.setHeaderText("Loading an image");
-        alert.setContentText("No images to load!");
-        alert.showAndWait();
-    }
 }
--- src/main/java/org/vl4ds4m/objdetjfx/Main.java
+++ src/main/java/org/vl4ds4m/objdetjfx/Main.java
@@ -2,52 +2,128 @@ package org.vl4ds4m.objdetjfx;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.layout.Pane;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
-/**
- * A main class to launch the application
- */
 public class Main extends Application {
     private static final String FXML_PATH = "/properties.fxml";
     private static final String TITLE = "Object Detection Application";
+    private final StackPane stackPane = new StackPane();
+    private final ImageView imageView = new ImageView();
+    private final Pane rectanglesPane = new Pane();
+    private boolean isRectanglesDrawn = false;
+    private final Button imageAdditionButton = new Button();
+    private final Button objectsDetectionButton = new Button();
 
-    /**
-     * Launch the application
-     *
-     * @param stage a main frame of the application
-     */
-    @Override
-    public void start(Stage stage) {
-        Pane root;
-
-        // Load layout properties from the FXML file
+    private GridPane loadRootLayout() {
         try {
             FXMLLoader loader = new FXMLLoader();
             loader.setLocation(getClass().getResource(FXML_PATH));
-            root = loader.load();
+            return loader.load();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private void handleImageLoader(Stage stage) {
+        imageAdditionButton.setOnAction(actionEvent -> {
+            FileChooser fileChooser = new FileChooser();
+            File imageFile = fileChooser.showOpenDialog(stage);
+            if (imageFile != null) {
+                try {
+                    Image image = new Image(new FileInputStream(imageFile.getAbsolutePath()));
+                    if (image.isError()) {
+                        showAlert("Can't open the file.", "The file isn't an image!");
+                    } else {
+                        imageView.setImage(image);
+                        rectanglesPane.getChildren().clear();
+                        isRectanglesDrawn = false;
+                    }
+                } catch (FileNotFoundException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+    }
+
+    private void handleObjectsStroking() {
+        objectsDetectionButton.setOnAction(actionEvent -> {
+            if (imageView.getImage() != null) {
+                if (!isRectanglesDrawn) {
+                    Rectangle rectangle = new Rectangle();
+                    rectangle.setX(10.0);
+                    rectangle.setY(100.0);
+                    rectangle.setWidth(200.0);
+                    rectangle.setHeight(200.0);
+                    rectangle.setFill(Color.TRANSPARENT);
+                    rectangle.setStroke(Color.RED);
+                    rectangle.setStrokeWidth(3.0);
+                    rectanglesPane.getChildren().add(rectangle);
+                    isRectanglesDrawn = true;
+                } else {
+                    showAlert("Stroking objects", "Objects have already been stroked!");
+                }
+            } else {
+                showAlert("Can't stroke objects.", "No image is selected!");
+            }
+        });
+    }
+
+    private void showAlert(String header, String message) {
+        Alert alert = new Alert(Alert.AlertType.WARNING);
+        alert.setHeaderText(header);
+        alert.setContentText(message);
+        alert.show();
+    }
+
+    @Override
+    public void start(Stage stage) {
+        // Create layers
+        GridPane root = loadRootLayout();
+        VBox buttonsBox = new VBox();
+        Label imageLabel = new Label("Your image");
+
+        // Adjust scene elements
+        imageAdditionButton.setText("Load an image");
+        objectsDetectionButton.setText("Stroke objects");
+        imageLabel.setFont(Font.font(20.0));
+        buttonsBox.setAlignment(Pos.CENTER);
+        buttonsBox.setSpacing(10.0);
+
+        stackPane.getChildren().addAll(imageLabel, imageView, rectanglesPane);
+        buttonsBox.getChildren().addAll(imageAdditionButton, objectsDetectionButton);
+        root.add(stackPane, 0, 0);
+        root.add(buttonsBox, 1, 0);
+
+        // Launch handlers
+        handleImageLoader(stage);
+        handleObjectsStroking();
 
         // Set a scene
         stage.setScene(new Scene(root));
         stage.setMinWidth(root.getPrefWidth());
         stage.setMinHeight(root.getPrefHeight());
         stage.setTitle(TITLE);
-        stage.show();
 
-        System.out.println("Hello world!");
+        stage.show();
     }
 
-    /**
-     * Launch the {@code start} method
-     *
-     * @param args launch parameters
-     */
     public static void main(String[] args) {
         Application.launch(args);
     }
