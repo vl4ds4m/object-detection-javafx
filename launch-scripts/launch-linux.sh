ce9194251c936d52b1995e2fd385f19de83535d1
Vladislav Samsonov <vl4d.s4m@gmail.com>
2023-05-07 11:53:30 +0300

    Add launch scripts

---
+++ launch-scripts/launch-linux.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+
+PATH=/usr/lib/jvm/jdk-17/bin:$PATH
+
+java --module-path \
+jar/object-detection-javafx-1.0-SNAPSHOT.jar:\
+jar/javafx-base-17.0.6-linux.jar:\
+jar/javafx-controls-17.0.6-linux.jar:\
+jar/javafx-fxml-17.0.6-linux.jar:\
+jar/javafx-graphics-17.0.6-linux.jar:\
+jar/opencv-4.7.0-0.jar \
+--module org.vl4ds4m.objdetjfx/org.vl4ds4m.objdetjfx.Main
