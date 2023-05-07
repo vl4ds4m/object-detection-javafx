aaa3528c15daba7ef8859bdfe439cdf9847c7361
vl4ds4m <98585027+vl4ds4m@users.noreply.github.com>
2023-05-07 12:42:09 +0300

    Update launch-windows.bat

--- launch-scripts/launch-windows.bat
+++ launch-scripts/launch-windows.bat
@@ -3,6 +3,6 @@ jar\object-detection-javafx-1.0-SNAPSHOT.jar;^
 jar\javafx-base-17.0.6-win.jar;^
 jar\javafx-controls-17.0.6-win.jar;^
 jar\javafx-fxml-17.0.6-win.jar;^
-jar\javafx-graphics-17.0.6-win.jar ^
--Djava.library.path=lib ^
+jar\javafx-graphics-17.0.6-win.jar;^
+jar\opencv-4.7.0-0.jar ^
 --module org.vl4ds4m.objdetjfx/org.vl4ds4m.objdetjfx.Main
