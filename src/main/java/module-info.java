1e6788c0989a56c385b9e273a226be19c365e992
Vladislav Samsonov <vl4d.s4m@gmail.com>
2023-05-13 11:27:52 +0300

    Add button to save processed image

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,9 @@ module org.vl4ds4m.objdetjfx {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.graphics;
+    requires javafx.swing;
     requires opencv;
+    requires java.desktop;
 
     opens org.vl4ds4m.objdetjfx;
 }
--- src/main/java/org/vl4ds4m/objdetjfx/Controller.java
+++ src/main/java/org/vl4ds4m/objdetjfx/Controller.java
@@ -1,16 +1,21 @@
 package org.vl4ds4m.objdetjfx;
 
+import javafx.embed.swing.SwingFXUtils;
 import javafx.fxml.FXML;
+import javafx.geometry.Rectangle2D;
+import javafx.scene.SnapshotParameters;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.image.WritableImage;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.FileChooser;
 
+import javax.imageio.ImageIO;
 import java.io.*;
 import java.util.List;
 
@@ -32,7 +37,6 @@ public class Controller {
     private String imageFileName = "";
     private boolean isBoundedBoxesDrawn = false;
     private List<ObjectData> objectDataList = List.of();
-    private static final String OUT_FILE_SUFFIX = "_labels.txt";
 
     @FXML
     private void loadImage() {
@@ -44,10 +48,12 @@ public class Controller {
                 Image image = new Image(new FileInputStream(imageFileName));
                 if (!image.isError()) {
                     if (Double.max(image.getWidth(), image.getHeight()) > 640.0) {
-                        showAlert("Loading an image",
+                        showAlert(Alert.AlertType.WARNING, "Loading an image",
                                 "The image resolution should be not greater than 640x640");
                     } else {
                         imageView.setImage(image);
+                        visualPane.setMaxHeight(image.getHeight());
+                        visualPane.setMaxWidth(image.getWidth());
                         boundedBoxesPane.setMaxHeight(image.getHeight());
                         boundedBoxesPane.setMaxWidth(image.getWidth());
                         boundedBoxesPane.getChildren().clear();
@@ -55,10 +61,10 @@ public class Controller {
                         isBoundedBoxesDrawn = false;
                     }
                 } else {
-                    showAlert("Loading an image", "The file isn't an image!");
+                    showAlert(Alert.AlertType.WARNING, "Loading an image", "The file isn't an image!");
                 }
             } catch (FileNotFoundException e) {
-                showAlert("Loading an image", "No image exists!");
+                showAlert(Alert.AlertType.WARNING, "Loading an image", "No image exists!");
             }
         }
     }
@@ -89,13 +95,15 @@ public class Controller {
                     objectsCounter.setText(String.valueOf(objectDataList.size()));
                     isBoundedBoxesDrawn = true;
                 } catch (IndexOutOfBoundsException | NumberFormatException e) {
-                    showAlert("Objects detection", "The data file has incorrect data!");
+                    showAlert(Alert.AlertType.WARNING,
+                            "Objects detection", "The data file has incorrect data!");
                 }
             } else {
-                showAlert("Objects detection", "Objects have already been stroked!");
+                showAlert(Alert.AlertType.WARNING,
+                        "Objects detection", "Objects have already been stroked!");
             }
         } else {
-            showAlert("Objects detection", "No image is selected!");
+            showAlert(Alert.AlertType.WARNING, "Objects detection", "No image is selected!");
         }
     }
 
@@ -103,31 +111,65 @@ public class Controller {
     private void writeLabelsToFile() {
         if (imageView.getImage() != null) {
             if (isBoundedBoxesDrawn) {
-                final String labelsFileName = imageFileName.split("\\.", 2)[0] + OUT_FILE_SUFFIX;
+                final String labelsFileName = getFileName() + "_labels.txt";
                 try (BufferedWriter writer = new BufferedWriter(new FileWriter(labelsFileName))) {
                     for (ObjectData objectData : objectDataList) {
-                        writer.write(objectData.X_CENTER() + " " +
-                                objectData.Y_CENTER() + " " +
-                                objectData.WIDTH() + " " +
-                                objectData.HEIGHT() + " " +
-                                objectData.confidence());
+                        writer.write(objectData.X_CENTER() + " " + objectData.Y_CENTER() + " " +
+                                objectData.WIDTH() + " " + objectData.HEIGHT() + " " +
+                                objectData.confidence() + " " + objectData.type());
                         writer.newLine();
                     }
+                    showAlert(Alert.AlertType.INFORMATION, "Writing labels",
+                            "Labels have been saved near to the original image!");
                 } catch (IOException e) {
-                    showAlert("Writing labels", "Can't write labels to the file!");
+                    showAlert(Alert.AlertType.WARNING,
+                            "Writing labels", "Can't write labels to the file!");
                 }
             } else {
-                showAlert("Writing labels", "Stroke objects before writing labels!");
+                showAlert(Alert.AlertType.WARNING,
+                        "Writing labels", "Stroke objects before writing labels!");
             }
         } else {
-            showAlert("Writing labels", "No image is selected!");
+            showAlert(Alert.AlertType.WARNING, "Writing labels", "No image is selected!");
         }
     }
 
-    private void showAlert(String header, String message) {
-        Alert alert = new Alert(Alert.AlertType.WARNING);
+    @FXML
+    private void saveProcessedImage() {
+        if (imageView.getImage() != null) {
+            if (isBoundedBoxesDrawn) {
+                SnapshotParameters snapshotParameters = new SnapshotParameters();
+                snapshotParameters.setViewport(new Rectangle2D(
+                        visualPane.getLayoutX(), visualPane.getLayoutY(),
+                        visualPane.getMaxWidth(), visualPane.getMaxHeight()));
+                WritableImage writableImage = visualPane.snapshot(snapshotParameters, null);
+                File imageFile = new File(getFileName() + "_processed.png");
+                try {
+                    ImageIO.write(SwingFXUtils.fromFXImage(writableImage, null),
+                            "png", imageFile);
+                    showAlert(Alert.AlertType.INFORMATION, "Saving the processed image",
+                            "The image have been saved near to the original image!");
+                } catch (IOException e) {
+                    showAlert(Alert.AlertType.WARNING,
+                            "Saving the processed image", "Can't save the image");
+                }
+            } else {
+                showAlert(Alert.AlertType.WARNING,
+                        "Saving the processed image", "Stroke objects before writing labels!");
+            }
+        } else {
+            showAlert(Alert.AlertType.WARNING, "Saving the processed image", "No image is selected!");
+        }
+    }
+
+    private void showAlert(Alert.AlertType alertType, String header, String message) {
+        Alert alert = new Alert(alertType);
         alert.setHeaderText(header);
         alert.setContentText(message);
         alert.show();
+    }
+
+    private String getFileName() {
+        return imageFileName.split("\\.", 2)[0];
     }
 }
