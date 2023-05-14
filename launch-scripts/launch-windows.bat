54db67c41bf8ce9949c8825a80c66e751adf2f93
Vladislav Samsonov <vl4d.s4m@gmail.com>
2023-05-14 14:35:56 +0300

    Add swing-dependency to win-launch script

--- launch-scripts/launch-windows.bat
+++ launch-scripts/launch-windows.bat
@@ -4,5 +4,6 @@ jar\javafx-base-17.0.6-win.jar;^
 jar\javafx-controls-17.0.6-win.jar;^
 jar\javafx-fxml-17.0.6-win.jar;^
 jar\javafx-graphics-17.0.6-win.jar;^
+jar\javafx-swing-17.0.6-win.jar;^
 jar\opencv-4.7.0-0.jar ^
 --module org.vl4ds4m.objdetjfx/org.vl4ds4m.objdetjfx.Main
